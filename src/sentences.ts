1b35c623f3ea5eb648ddf360fc28f090e7495233
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2024-05-22 16:41:46 -0600

    First questions

---
+++ src/sentences.ts
@@ -0,0 +1,16 @@
+export const sentences = [
+    {   question: "I _ a hammer.",
+        choices: ['can', 'think', 'need', 'go', 'do'],
+        answer: 'need'
+    },
+
+    {   question: "Please, _ the distance between those 2 posts.",
+    choices: ['can', 'think', 'cut', 'sand', 'measure'],
+    answer: 'measure'
+    },
+
+    {   question: "Can you please _ me this toolbox?",
+    choices: ['get', 'look for', 'cut', 'go', 'want'],
+    answer: 'get'
+    },
+]
