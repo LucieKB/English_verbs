4541ed8231dd6ccf2560ac9dd671f8004656aace
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2024-05-22 17:23:00 -0600

    disabling buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { sentences } from './sentences';
 function App() {
   const [score, setScore] = useState(0);
   const [currentSentence, setCurrentSentence] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false)
 
   const possibleAnswers = sentences[currentSentence].choices
   
@@ -12,29 +13,40 @@ function App() {
     if (ans === sentences[currentSentence].answer){
       setScore(score + 1);
       alert('Good Answer !');
-      setCurrentSentence(currentSentence + 1)
+      setIsDisabled(true);
     }
-
     else {
-      alert(`Wrong Answer, the correct answer was ${sentences[currentSentence].answer}`);
-      setCurrentSentence(currentSentence + 1)
+      alert(`Wrong Answer, the correct answer was ${sentences[currentSentence].answer.toUpperCase()}`);
+      setScore(score - 0.1)
     }
+  }
+
+  function onNext(){
+    setCurrentSentence(currentSentence + 1);
+    setIsDisabled(()=> !isDisabled)
   }
 
   return (
     <>
     <h1>Verb Quiz</h1>
     <div>
-      My Score : {score}
+      <h2>
+        My Score : {score}
+      </h2>
+      
     </div>
     
     <div>
-      {sentences[currentSentence].question}
+      <p style={{fontFamily:'sans-serif', fontSize:'30px'}}>
+        {sentences[currentSentence].question}
+      </p>
+      
     </div>
     <div>
     <ul>
-      {possibleAnswers.map((ans)=><button onClick = {() => onAnswer(ans)}>{ans}</button>)}
-      </ul>
+      {possibleAnswers.map((ans)=><button className="answers" disabled={isDisabled} key={sentences[currentSentence].choices.indexOf(ans)} onClick = {() => onAnswer(ans)}>{ans}</button>)}
+    </ul>
+    <button className="next" onClick = {()=> onNext()}>next</button>
     </div>
       
     
--- src/sentences.ts
+++ src/sentences.ts
@@ -1,15 +1,21 @@
 export const sentences = [
-    {   question: "I _ a hammer.",
-        choices: ['can', 'think', 'need', 'go', 'do'],
-        answer: 'need'
+    {   
+    id: 1,
+    question: "I _ a hammer.",
+    choices: ['can', 'think', 'need', 'go', 'do'],
+    answer: 'need'
     },
 
-    {   question: "Please, _ the distance between those 2 posts.",
+    {   
+    id: 2,
+    question: "Please, _ the distance between those 2 posts.",
     choices: ['can', 'think', 'cut', 'sand', 'measure'],
     answer: 'measure'
     },
 
-    {   question: "Can you please _ me this toolbox?",
+    {   
+    id: 3,
+    question: "Can you please _ me this toolbox?",
     choices: ['get', 'look for', 'cut', 'go', 'want'],
     answer: 'get'
     },
