a196c47ec776011b374d21ad382a72e59bca66de
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2024-05-22 18:38:16 -0600

    Answer showing up

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,74 @@ import { useState } from 'react'
 import './App.css'
 import { sentences } from './sentences';
 
+let leftAttempts = 3;
+
 function App() {
   const [score, setScore] = useState(0);
   const [currentSentence, setCurrentSentence] = useState(0);
   const [isDisabled, setIsDisabled] = useState(false)
+  const [myQuestion, setMyQuestion] = useState(sentences[currentSentence].question)
 
-  const possibleAnswers = sentences[currentSentence].choices
+  const possibleAnswers = sentences[currentSentence].choices;
+  let attemptColor;
+  if (leftAttempts === 1){
+    attemptColor = "red"}
+  else{
+    attemptColor = "green"
+  }
   
-  function onAnswer(ans:String){
-    if (ans === sentences[currentSentence].answer){
+
+  function onAnswer(ans: String) {
+    if (ans === sentences[currentSentence].answer) {
       setScore(score + 1);
+      setMyQuestion(myQuestion.replace("_", `${ans}`));
       alert('Good Answer !');
       setIsDisabled(true);
     }
-    else {
-      alert(`Wrong Answer, the correct answer was ${sentences[currentSentence].answer.toUpperCase()}`);
+    else if (ans !== sentences[currentSentence].answer && leftAttempts > 1) {
+      leftAttempts = leftAttempts - 1;
+      console.log(leftAttempts)
       setScore(score - 0.1)
+    }
+
+    else {
+      setIsDisabled(true);
+      leftAttempts = leftAttempts - 1;
+      setMyQuestion(myQuestion.replace("_", `${ans}`));
+      alert(`Wrong Answer, the correct answer was ${sentences[currentSentence].answer.toUpperCase()}`)
     }
   }
 
-  function onNext(){
+  function onNext() {
     setCurrentSentence(currentSentence + 1);
-    setIsDisabled(()=> !isDisabled)
+    setMyQuestion(sentences[currentSentence +1].question)
+    setIsDisabled(() => !isDisabled);
+    leftAttempts = 3;
   }
 
   return (
     <>
-    <h1>Verb Quiz</h1>
-    <div>
-      <h2>
-        My Score : {score}
-      </h2>
-      
-    </div>
-    
-    <div style={{marginTop:'150px'}}>
-      <p style={{fontFamily:'sans-serif', fontSize:'30px'}}>
-        {sentences[currentSentence].question}
-      </p>
-      <ul>
-      {possibleAnswers.map((ans)=><button className="answers" disabled={isDisabled} key={sentences[currentSentence].choices.indexOf(ans)} onClick = {() => onAnswer(ans)}>{ans}</button>)}
-    </ul>
-    </div>
-    <div>
-    
-    <button className="next" onClick = {()=> onNext()}>next</button>
-    </div>
-      
-    
+      <h1>Verb Quiz</h1>
+      <div>
+        <h2>
+          My Score : {score}
+        </h2>
+
+      </div>
+
+      <div style={{ marginTop: '150px' }}>
+        <p style={{ fontFamily: 'sans-serif', fontSize: '30px' }}>
+          {myQuestion}
+        </p>
+        <ul>
+          {possibleAnswers.map((ans) => <button className="answers" disabled={isDisabled} key={sentences[currentSentence].choices.indexOf(ans)} onClick={() => onAnswer(ans)}>{ans}</button>)}
+        </ul>
+        <p style={{color:`${attemptColor}`}}><i><u>Attemps left:</u> {leftAttempts}</i></p>
+      </div>
+
+      <div>
+        <button className="next" onClick={() => onNext()}>next</button>
+      </div>
     </>
   )
 }
--- src/sentences.ts
+++ src/sentences.ts
@@ -1,22 +1,26 @@
 export const sentences = [
     {   
-    id: 1,
     question: "I _ a hammer.",
     choices: ['can', 'think', 'need', 'go', 'do'],
     answer: 'need'
     },
 
     {   
-    id: 2,
     question: "Please, _ the distance between those 2 posts.",
     choices: ['can', 'think', 'cut', 'sand', 'measure'],
     answer: 'measure'
     },
 
     {   
-    id: 3,
     question: "Can you please _ me this toolbox?",
     choices: ['get', 'look for', 'cut', 'go', 'want'],
     answer: 'get'
     },
+
+    {   
+        question: "I _ my tools, I don't know where they are.",
+        choices: ['look for', 'am looking for', 'find', 'am finding'],
+        answer: 'am looking for'
+        },
+
 ]
